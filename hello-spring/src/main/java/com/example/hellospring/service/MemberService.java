43f37b039816ee5c11a471858ef038f7ee8f2e9f
seungm2n2 <seungm2n2@gmail.com>
2023-11-13 21:54:08 +0900

    Member Repository TEST CODE 작성 및 회원가입시 중복회원 validate

---
+++ hello-spring/src/main/java/com/example/hellospring/service/MemberService.java
@@ -0,0 +1,39 @@
+package com.example.hellospring.service;
+
+import com.example.hellospring.domain.Member;
+import com.example.hellospring.repository.MemberRepository;
+import com.example.hellospring.repository.MemoryMemberRepository;
+
+import java.util.List;
+import java.util.Optional;
+
+public class MemberService {
+
+    private final MemberRepository memberRepository = new MemoryMemberRepository();
+
+    /*
+    * 회원가입
+    * */
+    public Long join(Member member) {
+
+        validateDuplicateMember(member); // 중복 회원
+
+        memberRepository.save(member);
+        return member.getId();
+    }
+
+    private void validateDuplicateMember(Member member) {
+        memberRepository.findByName(member.getName())
+                .ifPresent(m -> {
+                    throw new IllegalStateException("이미 존재하는 회원입니다.");
+                });
+    }
+
+    public List<Member> findMembers() {
+        return memberRepository.findAll();
+    }
+
+    public Optional<Member> findOne(Long memberId) {
+        return memberRepository.findById(memberId);
+    }
+}
